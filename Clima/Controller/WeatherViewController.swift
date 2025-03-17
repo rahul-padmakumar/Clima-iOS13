a4b0c6f0836eb7da0d183f38b76cc6bc23e1a1d6
rahulp <rahulp@qburst.com>
2025-03-17 18:50:51 +0530

    Complete text field functionlity

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var searchTF: UITextField!
     @IBOutlet weak var cityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTF.delegate = self
     }
-
-
+    
+    @IBAction func onSearchPressed(_ sender: UIButton) {
+        search()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        search()
+        return true
+    }
+    
+    func search(){
+        print(searchTF.text!)
+        searchTF.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTF.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTF.text == ""{
+            searchTF.placeholder = "Please type something"
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
